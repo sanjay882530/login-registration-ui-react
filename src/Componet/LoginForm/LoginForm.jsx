c73ba66046ae1a2183d374993d562c1072e53126
sanjay882530 <sanjaypd7293@gmail.com>
2024-02-20 16:41:08 +0530

    Signup side changes

--- src/Componet/LoginForm/LoginForm.jsx
+++ src/Componet/LoginForm/LoginForm.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { FaLock, FaUser } from "react-icons/fa";
 import "./LoginForm.css";
 import "./RegistrationForm";
+import ReCAPTCHA from "react-google-recaptcha";
+
+function onChange(value) {
+  console.log("Captcha value:", value);
+}
 const LoginForm = () => {
   return (
     <div className='wrapperForm'>  
@@ -18,17 +23,20 @@ const LoginForm = () => {
      
         </div>
         <div className='rember-me'>
-            <label><input type='checkbox'/>Rember me</label>
-            <a href='#'>Forget Password?</a>
+            <label><input type='checkbox'/>Rember me</label><a href='#'>Forget Password?</a>
         </div>
-
+          <div className='recapcha-key'><ReCAPTCHA
+            sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            onChange={onChange}
+         /></div>
         <button type='submit'>Login</button>
         <div className='register-me'>
-            <p>Don't have an accout?<a href="/RegistrationForm">Register here</a></p>
+            <label>Don't have an accout? </label><a href="/RegistrationForm"> Register here</a>
         </div>
      </form>
     </div>
   )
 }
+
 
 export default LoginForm
--- src/Componet/LoginForm/RegistrationForm.jsx
+++ src/Componet/LoginForm/RegistrationForm.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
-
+import ReCAPTCHA from "react-google-recaptcha";
+import "./RegistrationForm.css";
+import { IoMail } from "react-icons/io5";
+import { CgOrganisation } from "react-icons/cg";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { MdOutlinePhoneIphone } from "react-icons/md";
+import { VscOrganization } from "react-icons/vsc";
+function onChange(value) {
+  console.log("Captcha value:", value);
+}
 function RegistrationForm() {
   return (
-      <div className='wrapperForm'>  
+      <div className='wrapperFormReg'>  
      <form action=''>
         <h1>Register to welcome application</h1>
-        <div className='input-form'><input  type='text' placeholder='first name'></input></div>
-        <div className='input-form'><input  type='text' placeholder='last name'></input></div>
-        <div className='input-form'> <input  type='text' placeholder='emailId'></input> </div>
-        <div className='input-form'> <input  type='text' placeholder='mobileNo'></input> </div>
-        <div className='input-form'> <input  type='text' placeholder='designation'></input> </div>
-        <div className='input-form'><label><input  type='checkbox' placeholder='designation'></input> all are correct?</label></div>
+
+        <div className='input-form'><CgOrganisation className='icon' /><input  type='text' placeholder='organazation name'></input></div>
+        <div className='fname'><MdDriveFileRenameOutline className='icon' /><input  type='text' placeholder='first name'></input>
+        <MdDriveFileRenameOutline className='icon' /><input  type='text' placeholder='last name'></input></div>
+        <div className='input-form'> <IoMail className='icon' /><input  type='text' placeholder='emailId'></input> </div>
+        <div className='input-form'> <MdOutlinePhoneIphone className='icon'/><input  type='text' placeholder='contactNo'></input> </div>
+        <div className='input-form'> <VscOrganization className='icon'/><input  type='text' placeholder='designation'></input> </div>
+        <div className='check-form'><label><input  type='checkbox'></input> I read and agree to the Terms of use & Privacy policy.?</label></div>
+        <div className='recapcha-key'><ReCAPTCHA sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            onChange={onChange}
+         /></div>
         <div className='input-form'> <button  type='submit' >Register</button> </div>
        </form>
     </div>
